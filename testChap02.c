ae5d377a236f40dae70dca87fb54effaa45c21b3
thurizas <thurizas123@gmail.com>
2025-03-17 18:51:11 -0400

    added two test cases to testChap02.c that fail

--- testChap02.c
+++ testChap02.c
@@ -1,5 +1,9 @@
 int main(void)
 {
-    return 5*(3+2);
-	//return -5 * (13 + -4) / 14 % 5;
+	return ~(-5);
+    //return 3 + 5*2;                 // fails @ '*' token
+	//return 5*3+2;                   // produces 2,3,+,5,*, return
+	//return 5*(3+2);                 // produces 2,3,+,5,*, return
+    //return (3+2)*5;                 // produces 5,2,3,+,*, return
+	//return -5 * (13 + -4) / 14 % 5; // fails @ '/' token
 }
